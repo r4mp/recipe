bbfe667d4400bf2ea75bb010ccdc9ecdb62d31b6
r4mp <r4mp@chaosdorf.de>
2013-06-20 18:36:53 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recipe::Application.config.secret_token = '646c964aad0b150f34bf56094659c092a6189076dc4b641faa5ea348f6bca0b5b1c46ebd4f63925f5d28fe188b5758ee1711c3947f0f23c26ebc6ce581bd6f45'
