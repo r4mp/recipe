19c0fdfc8c184e9d85e64983b80769d9cfb32a23
r4mp <r4mp@chaosdorf.de>
2013-06-25 16:15:23 +0200

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Recipe::Application.routes.draw do
+  get "ingredient/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -9,6 +11,8 @@ Recipe::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
+
+  root :to => "ingredient#index"
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
